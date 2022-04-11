6ea91a3c262680312e8ea4865427dc8b66640acd
Wayldr <steinerjules@gmail.com>
2022-04-11 09:46:16 +0200

    add reservation

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const parkings = require('./parkings.json')
 
 app.use(express.json())
 
+app.listen(8080, () => {
+    console.log("Serveur à l'écoute")
+})
+
+/* ------------------------------------------------------------------------------parkings */
 app.get('/parkings', (req,res) => {
     res.status(200).json(parkings)
 })
-
 app.post('/parkings', (req,res) => {
     parkings.push(req.body)
     res.status(200).json(parkings)
+})
+app.put('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    let parking = parkings.find(parking => parking.id === id)
+    parking.name =req.body.name,
+    parking.city =req.body.city,
+    parking.type =req.body.type,
+    res.status(200).json(parking)
 })
 
 app.get('/parkings/:id', (req,res) => {
@@ -18,7 +30,15 @@ app.get('/parkings/:id', (req,res) => {
     const parking = parkings.find(parking => parking.id === id)
     res.status(200).json(parking)
 })
-
-app.listen(8080, () => {
-    console.log("Serveur à l'écoute")
+app.delete('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    let parking = parkings.find(parking => parking.id === id)
+    parkings.splice(parkings.indexOf(parking),1)
+    res.status(200).json(parkings)
 })
+/* --------------------------------------------------------------------------Reservation */
+app.get('/reservations', (req,res) => {
+    res.status(200).json(reservations)
+})
+
+
